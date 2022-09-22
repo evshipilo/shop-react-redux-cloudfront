6ce69488e7847da19e38ae8a7773b772249f2fe1
Yauheni Shypila <Yauheni_Shypila@epam.com>
2022-09-22 18:00:19 +0300

    feat: implemented 5 task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pyg0xu0lv9.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://tqveisy986.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
